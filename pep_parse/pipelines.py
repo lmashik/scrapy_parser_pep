1eac4a7265b3f67c279a4850efb0142d78583eb7
Мария Лапикова <mashik_p@mail.ru>
2023-06-11 22:23:27 +0300

    parsing method pep; updated items; created pipline for counting statuses; added settings for saving main file

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,28 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import datetime as dt
+from collections import defaultdict
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from .settings import BASE_DIR
 
 
 class PepParsePipeline:
+    status_dict = defaultdict(int)
+    total = 0
+
+    def open_spider(self, spider):
+        pass
+
     def process_item(self, item, spider):
+        self.total += 1
+        self.status_dict[item['status']] += 1
         return item
+
+    def close_spider(self, spider):
+        current_datetime = str(dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        filename = 'results/status_summary_' + current_datetime + '.csv'
+        with open(
+            BASE_DIR / filename, mode='w', encoding='utf-8'
+        ) as f:
+            f.write('Статус,Количество\n')
+            for status in self.status_dict:
+                f.write(f'{status},{self.status_dict[status]}\n')
+            f.write(f'Total,{self.total}\n')
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,38 @@
+import re
 import scrapy
+
+from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
+    custom_settings = {
+        'FEED_EXPORT_FIELDS': ['Номер', 'Название', 'Статус'],
+    }
 
     def parse(self, response):
-        pass
+        numerical_index = response.xpath('//section[@id="numerical-index"]')
+        peps = numerical_index.css('tbody tr a::attr(href)').getall()
+        for pep_link in peps:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        pattern = r'PEP (?P<number>\d+) – (?P<name>.*)'
+        # title = response.css('article h1::text').get()
+        title = ' '.join(
+            t.strip() for t in response.css('article h1::text').getall()
+        ).strip()
+        number, name = (item.strip() for item in title.split('–'))
+        # text_match = re.search(pattern, title)
+        # number, name = text_match.groups()
+        status = response.xpath(
+            '//*[contains(., "Status")]/following-sibling::*/child::*/text()'
+        ).get()
+        data = {
+            'number': number,
+            'name': name,
+            'status': status,
+        }
+        yield PepParseItem(data)
